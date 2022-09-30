6e2608898c435a09aec2433629dfb224f7cbaef4
Joern Bernhardt <joern.bernhardt@compose.us>
2022-09-30 19:01:16 +0200

    feat: Add individual posts
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,7 @@
 import path from "node:path";
 import { mkdir, writeFile } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
-import posts from "../database/articles.json" assert { type: "json" };
+import articles from "../database/articles.json" assert { type: "json" };
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const buildDir = path.resolve(dirname, "../build");
@@ -21,10 +21,27 @@ const createIndexPage = (posts) => `
     ${posts
       .filter((post) => post.status === "done")
       .map((post) => {
-        return `<li>${post.title}</li>`;
+        return `<li><a href="${post.id}/">${post.title}</a></li>`;
       })
       .join("")}
     </ul>
+</body>
+</html>
+`;
+
+const createPostPage = (post) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${post.title}</title>
+</head>
+<body>
+    <h1>${post.title}</h1>
+    ${post.body}
+    <footer><a href="..">Back to index</a></footer>
 </body>
 </html>
 `;
@@ -38,5 +55,20 @@ run()
 
 async function run() {
   await mkdir(buildDir, { recursive: true });
-  await writeFile(`${buildDir}/index.html`, createIndexPage(posts));
+  await writeFile(`${buildDir}/index.html`, createIndexPage(articles));
+
+  const posts = articles
+    .filter((post) => post.status === "done")
+    .map((post) => {
+      const postDir = `${buildDir}/${post.id}`;
+      return {
+        path: postDir,
+        content: createPostPage(post),
+      };
+    });
+
+  for await (const post of posts) {
+    await mkdir(post.path, { recursive: true });
+    await writeFile(`${post.path}/index.html`, post.content);
+  }
 }
